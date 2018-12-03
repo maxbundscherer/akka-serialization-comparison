b1db3ffcaf724c7d314616af879a68be2d39ab0d
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2018-12-03 11:52:14 +0100

    Add DB Models

--- src/main/scala/de/maxbundscherer/akka/serializationcomparision/serializer/JsonSerializer.scala
+++ src/main/scala/de/maxbundscherer/akka/serializationcomparision/serializer/JsonSerializer.scala
@@ -11,6 +11,45 @@ class JsonSerializer extends SerializerWithStringManifest {
   import de.maxbundscherer.akka.serializationcomparision.persistence.CarGarageAggregate._
 
   /**
+    * Models
+    */
+  case class CarDb(
+                    id: Int,
+                    horsepower: Int,
+                    name: String
+                  )
+  case class AddCarEvtDb      (value: CarDb)
+  case class UpdateCarEvtDb   (value: CarDb)
+  case class CarGarageStateDb (cars: Vector[CarDb])
+
+  /**
+    * Converter
+    */
+  private object Converter {
+
+    object FromDbToEntity {
+
+      def car       (dbEntity: CarDb): Car = Car(id = dbEntity.id, horsepower = dbEntity.horsepower, name = dbEntity.name)
+
+      def addCarEvt     (dbEntity: AddCarEvtDb)     : AddCarEvt      = AddCarEvt      ( value = car(dbEntity.value) )
+      def updateCarEvt  (dbEntity: UpdateCarEvtDb)  : UpdateCarEvt   = UpdateCarEvt   ( value = car(dbEntity.value) )
+      def carGarageState(dbEntity: CarGarageStateDb): CarGarageState = CarGarageState ( cars = dbEntity.cars.map(c => car(c)) )
+
+    }
+
+    object FromEntityToDb {
+
+      def car       (entity: Car): CarDb = CarDb(id = entity.id, horsepower = entity.horsepower, name = entity.name)
+
+      def addCarEvt     (entity: AddCarEvt)     : AddCarEvtDb      = AddCarEvtDb      ( value = car(entity.value) )
+      def updateCarEvt  (entity: UpdateCarEvt)  : UpdateCarEvtDb   = UpdateCarEvtDb   ( value = car(entity.value) )
+      def carGarageState(entity: CarGarageState): CarGarageStateDb = CarGarageStateDb ( cars = entity.cars.map(c => car(c)) )
+
+    }
+
+  }
+
+  /**
     * Charset
     */
   final val UTF_8 = StandardCharsets.UTF_8.name()
@@ -18,9 +57,9 @@ class JsonSerializer extends SerializerWithStringManifest {
   /**
     * Manifests
     */
-  final val AddCarEvtManifest       = classOf[AddCarEvt]      .getName
-  final val UpdateCarEvtManifest    = classOf[UpdateCarEvt]   .getName
-  final val CarGarageStateManifest  = classOf[CarGarageState] .getName
+  final val AddCarEvtDbManifest       = classOf[AddCarEvtDb]      .getName
+  final val UpdateCarEvtDbManifest    = classOf[UpdateCarEvtDb]   .getName
+  final val CarGarageStateDbManifest  = classOf[CarGarageStateDb] .getName
 
   /**
     * Serializer Identifier
@@ -42,9 +81,20 @@ class JsonSerializer extends SerializerWithStringManifest {
     */
   override def toBinary(o: AnyRef): Array[Byte] = o match {
 
-    case AddCarEvt(_)       =>   o.asInstanceOf[AddCarEvt]      .asJson.toString().getBytes(UTF_8)
-    case UpdateCarEvt(_)    =>   o.asInstanceOf[UpdateCarEvt]   .asJson.toString().getBytes(UTF_8)
-    case CarGarageState(_)  =>   o.asInstanceOf[CarGarageState] .asJson.toString().getBytes(UTF_8)
+    case o: AddCarEvt      =>
+
+      val value: AddCarEvtDb = Converter.FromEntityToDb.addCarEvt(o)
+      value.asJson.toString().getBytes(UTF_8)
+
+    case o: UpdateCarEvt      =>
+
+      val value: UpdateCarEvtDb = Converter.FromEntityToDb.updateCarEvt(o)
+      value.asJson.toString().getBytes(UTF_8)
+
+    case o: CarGarageState      =>
+
+      val value: CarGarageStateDb = Converter.FromEntityToDb.carGarageState(o)
+      value.asJson.toString().getBytes(UTF_8)
 
     case any: Any => throw new NotImplementedError("Can not serialize '" + any + "'")
 
@@ -58,10 +108,20 @@ class JsonSerializer extends SerializerWithStringManifest {
     */
   override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = manifest match {
 
+    case AddCarEvtDbManifest      =>
 
-    case AddCarEvtManifest      => decode[AddCarEvt]      (new String(bytes, UTF_8)).right.get
-    case UpdateCarEvtManifest   => decode[UpdateCarEvt]   (new String(bytes, UTF_8)).right.get
-    case CarGarageStateManifest => decode[CarGarageState] (new String(bytes, UTF_8)).right.get
+      val value: AddCarEvtDb = decode[AddCarEvtDb](new String(bytes, UTF_8)).right.get
+      Converter.FromDbToEntity.addCarEvt(value)
+
+    case UpdateCarEvtDbManifest   =>
+
+      val value: UpdateCarEvtDb = decode[UpdateCarEvtDb](new String(bytes, UTF_8)).right.get
+      Converter.FromDbToEntity.updateCarEvt(value)
+
+    case CarGarageStateDbManifest =>
+
+      val value: CarGarageStateDb = decode[CarGarageStateDb](new String(bytes, UTF_8)).right.get
+      Converter.FromDbToEntity.carGarageState(value)
 
     case any: Any => throw new NotImplementedError("Can not deserialize '" + any + "'")
 
