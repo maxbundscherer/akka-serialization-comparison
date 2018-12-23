030af623a82efbd29d53a453afff356ce714d6ba
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2018-12-23 19:02:05 +0100

    Add ComplexCar and Implement Protobuf (complexCar)

--- src/main/scala/de/maxbundscherer/akka/serializationcomparision/persistence/CarGarageAggregate.scala
+++ src/main/scala/de/maxbundscherer/akka/serializationcomparision/persistence/CarGarageAggregate.scala
@@ -14,12 +14,30 @@ object CarGarageAggregate {
                   name: String
                 )
 
+  case class ComplexCar(
+                  id: Int,
+                  horsepower: Int,
+                  name: String,
+                  fuelConsumption: Float,
+                  dieselEngine: Boolean,
+                  seatAdjustment: Boolean,
+                  fuelTank: Double,
+                  brakingDistance: Double,
+                  notes: String
+                )
+
   // ~ Command and Evt ~
   case class AddCarCmd(value: Car)            extends CarGarageCmd
   case class AddCarEvt(value: Car)            extends CarGarageEvt
 
+  case class AddComplexCarCmd(value: ComplexCar) extends CarGarageCmd
+  case class AddComplexCarEvt(value: ComplexCar) extends CarGarageEvt
+
   case class UpdateCarCmd(value: Car)         extends CarGarageCmd
   case class UpdateCarEvt(value: Car)         extends CarGarageEvt
+
+  case class UpdateComplexCarCmd(value: ComplexCar) extends CarGarageCmd
+  case class UpdateComplexCarEvt(value: ComplexCar) extends CarGarageEvt
 
   // ~ Response ~
   case class CarGarageSuccess()               extends CarGarageResponse
@@ -32,5 +50,8 @@ object CarGarageAggregate {
   // ~ Request and Response
   case class GetAllCarCmd()                   extends CarGarageCmd
   case class GetAllCar(value: Vector[Car])    extends CarGarageResponse
+
+  case class GetAllComplexCarCmd()                   extends CarGarageCmd
+  case class GetAllComplexCar(value: Vector[ComplexCar])    extends CarGarageResponse
 
 }
--- src/main/scala/de/maxbundscherer/akka/serializationcomparision/serializer/ProtobufSerializer.scala
+++ src/main/scala/de/maxbundscherer/akka/serializationcomparision/serializer/ProtobufSerializer.scala
@@ -21,20 +21,25 @@ class ProtobufSerializer extends AbstractSerializer(serializerIdentifier = 9003)
     object FromDbToEntity {
 
       def car       (dbEntity: CarDb): Car = Car(id = dbEntity.id, horsepower = dbEntity.horsepower, name = dbEntity.name)
+      def complexCar(dbEntity: ComplexCarDb): ComplexCar = ComplexCar(id = dbEntity.id, horsepower = dbEntity.horsepower, name = dbEntity.name, fuelConsumption = dbEntity.fuelConsumption, dieselEngine = dbEntity.dieselEngine, seatAdjustment = dbEntity.seatAdjustment, fuelTank = dbEntity.fuelTank, brakingDistance = dbEntity.brakingDistance, notes = dbEntity.notes)
 
       def addCarEvt     (dbEntity: AddCarEvtDb)     : AddCarEvt      = AddCarEvt      ( value = car(dbEntity.value) )
       def updateCarEvt  (dbEntity: UpdateCarEvtDb)  : UpdateCarEvt   = UpdateCarEvt   ( value = car(dbEntity.value) )
-      def carGarageState(dbEntity: CarGarageStateDb): CarGarageState = CarGarageState ( cars = dbEntity.cars.map(c => car(c)).toVector )
+      def addComplexCarEvt     (dbEntity: AddComplexCarEvtDb)     : AddComplexCarEvt      = AddComplexCarEvt      ( value = complexCar(dbEntity.value) )
+      def updateComplexCarEvt  (dbEntity: UpdateComplexCarEvtDb)  : UpdateComplexCarEvt   = UpdateComplexCarEvt   ( value = complexCar(dbEntity.value) )
+      def carGarageState(dbEntity: CarGarageStateDb): CarGarageState = CarGarageState ( cars = dbEntity.cars.map(c => car(c)).toVector, complexCars = dbEntity.complexCars.map(c => complexCar(c)).toVector )
 
     }
 
     object FromEntityToDb {
 
       def car       (entity: Car): CarDb = CarDb(id = entity.id, horsepower = entity.horsepower, name = entity.name)
-
+      def complexCar(entity: ComplexCar): ComplexCarDb = ComplexCarDb(id = entity.id, horsepower = entity.horsepower, name = entity.name, fuelConsumption = entity.fuelConsumption, dieselEngine = entity.dieselEngine, seatAdjustment = entity.seatAdjustment, fuelTank = entity.fuelTank, brakingDistance = entity.brakingDistance, notes = entity.notes)
       def addCarEvt     (entity: AddCarEvt)     : AddCarEvtDb      = AddCarEvtDb      ( value = car(entity.value) )
       def updateCarEvt  (entity: UpdateCarEvt)  : UpdateCarEvtDb   = UpdateCarEvtDb   ( value = car(entity.value) )
-      def carGarageState(entity: CarGarageState): CarGarageStateDb = CarGarageStateDb ( cars = entity.cars.map(c => car(c)) )
+      def addComplexCarEvt     (entity: AddComplexCarEvt)     : AddComplexCarEvtDb      = AddComplexCarEvtDb      ( value = complexCar(entity.value) )
+      def updateComplexCarEvt  (entity: UpdateComplexCarEvt)  : UpdateComplexCarEvtDb   = UpdateComplexCarEvtDb   ( value = complexCar(entity.value) )
+      def carGarageState(entity: CarGarageState): CarGarageStateDb = CarGarageStateDb ( cars = entity.cars.map(c => car(c)), complexCars = entity.complexCars.map(c => complexCar(c)))
 
     }
 
@@ -55,6 +60,16 @@ class ProtobufSerializer extends AbstractSerializer(serializerIdentifier = 9003)
     case o: UpdateCarEvt      =>
 
       val value: UpdateCarEvtDb = Converter.FromEntityToDb.updateCarEvt(o)
+      value.toByteArray
+
+    case o: AddComplexCarEvt         =>
+
+      val value: AddComplexCarEvtDb = Converter.FromEntityToDb.addComplexCarEvt(o)
+      value.toByteArray
+
+    case o: UpdateComplexCarEvt      =>
+
+      val value: UpdateComplexCarEvtDb = Converter.FromEntityToDb.updateComplexCarEvt(o)
       value.toByteArray
 
     case o: CarGarageState    =>
@@ -83,6 +98,16 @@ class ProtobufSerializer extends AbstractSerializer(serializerIdentifier = 9003)
 
       val value: UpdateCarEvtDb = UpdateCarEvtDb.parseFrom(bytes)
       Converter.FromDbToEntity.updateCarEvt(value)
+
+    case AddComplexCarEvtManifest      =>
+
+      val value: AddComplexCarEvtDb = AddComplexCarEvtDb.parseFrom(bytes)
+      Converter.FromDbToEntity.addComplexCarEvt(value)
+
+    case UpdateComplexCarEvtManifest   =>
+
+      val value: UpdateComplexCarEvtDb = UpdateComplexCarEvtDb.parseFrom(bytes)
+      Converter.FromDbToEntity.updateComplexCarEvt(value)
 
     case CarGarageStateManifest =>
 
