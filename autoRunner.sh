f61bdad555ac0ad181560d25f860347d86c42318
maximilianbundscherer <maximilian@bundscherer-online.de>
2019-01-03 14:20:36 +0100

    Improve AutoRunner ec2

--- autoRunner.sh
+++ autoRunner.sh
@@ -1,6 +1,7 @@
-export SBT_OPTS="-Xms1G -Xmx8G";
-cat autoRunner.sh >> ~/Desktop/params.txt;
-cat src/main/resources/params.conf >> ~/Desktop/params.txt;
-sbt clean run >> ~/Desktop/run.log;
-sbt clean test >> ~/Desktop/test.log;
+export SBT_OPTS="-Xms1G -Xmx6G";
+mkdir results;
+cat autoRunner.sh >> results/params.txt;
+cat src/main/resources/params.conf >> results/params.txt;
+sbt clean run >> results/run.log;
+sbt clean test >> results/test.log;
 sudo shutdown now;
